74b52bc6ffdd6141dd62520ab8788e2e2638ed74
goldstar611 <goldstar611@users.noreply.github.com>
2022-09-14 09:25:25 -0500

    Update build.sh to move version number into variable Bump buildroot version Remove deprecated config options  - Bump toolchain kernel headers from 4.1 to 4.4 git ignore some files Use wget noclobber option

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -xe
 
-wget https://buildroot.org/downloads/buildroot-2018.02.7.tar.gz
-wget https://buildroot.org/downloads/buildroot-2018.02.7.tar.gz.sign
+version=2022.02.5
+
+wget -nc https://buildroot.org/downloads/buildroot-${version}.tar.gz
+wget -nc https://buildroot.org/downloads/buildroot-${version}.tar.gz.sign
 
 gpg --keyserver keyserver.ubuntu.com --recv-keys B025BA8B59C36319
-gpg --verify buildroot-2018.02.7.tar.gz.sign
+gpg --verify buildroot-${version}.tar.gz.sign
 
-tar xf buildroot-2018.02.7.tar.gz
-mv buildroot-2018.02.7 toolchain/
-cp mybooklive_toolchain_defconfig toolchain/buildroot-2018.02.7/configs/
+tar xf buildroot-${version}.tar.gz
+mv buildroot-${version}/ toolchain/
+cp mybooklive_toolchain_defconfig toolchain/configs/
 
-cd toolchain/buildroot-2018.02.7
-  make mybooklive_toolchain_defconfig
-  make
+(cd toolchain; make mybooklive_toolchain_defconfig)
+(cd toolchain; make)
 
